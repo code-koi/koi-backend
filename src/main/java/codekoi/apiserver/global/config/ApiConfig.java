155b2ec35f75e4bb362277b9902f45d5976f1b46
sdcodebase <sdcodebase@gmail.com>
2023-06-16 23:06:47 +0900

    Fix: ArgumentResolver 등록

---
+++ src/main/java/codekoi/apiserver/global/config/ApiConfig.java
@@ -0,0 +1,24 @@
+package codekoi.apiserver.global.config;
+
+import codekoi.apiserver.global.token.AuthenticationPrincipalArgumentResolver;
+import codekoi.apiserver.global.token.JwtTokenProvider;
+import codekoi.apiserver.global.token.NullablePrincipalArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ApiConfig implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthenticationPrincipalArgumentResolver(jwtTokenProvider));
+        resolvers.add(new NullablePrincipalArgumentResolver(jwtTokenProvider));
+    }
+}
