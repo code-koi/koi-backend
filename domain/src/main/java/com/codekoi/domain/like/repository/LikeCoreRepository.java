c11c6bd12b802d12a32df147bdd8ac2362e3efab
sdcodebase <sdcodebase@gmail.com>
2023-07-23 20:37:58 +0900

    Refactor: *CoreRepository에서 사용하지 않는 메서드 제거

--- domain/src/main/java/com/codekoi/domain/like/repository/LikeCoreRepository.java
+++ domain/src/main/java/com/codekoi/domain/like/repository/LikeCoreRepository.java
@@ -2,8 +2,6 @@ package com.codekoi.domain.like.repository;
 
 import com.codekoi.domain.like.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
